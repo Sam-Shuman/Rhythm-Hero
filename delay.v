c057a9b0d3a9c8c4c73bf4275bb35804b815d3bd
Sean Whyle <153943612+Sean-Whyle@users.noreply.github.com>
2023-12-15 18:42:28 -0500

    Add files via upload
    updated delay with comments in the code

--- delay.v
+++ delay.v
@@ -4,10 +4,17 @@ input rst;
 input en;
 output reg done;
 
+//sets a parameter for how long a second is which is based on the 
+//clock for this project which is 50 million hz
 parameter one_second = 26'd50000000;
 
 reg [25:0]counter;
 
+/*
+This seqential always block sets the value of counter to 0 until reset 
+is on in which counter counts each clock cycle, adding 1 to the counter
+until it reaches the parameter, sending a done signal to guitar.
+*/
 always @ (posedge clk or negedge rst)
 	begin 
 	if (rst == 1'b0)
